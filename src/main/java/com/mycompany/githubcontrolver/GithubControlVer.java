6603303ffa4df1eb902ec5af09a61b694611b77d
Raul60271 <raulsanromanduran@gmail.com>
2025-02-21 17:15:22 +0100

    Update GithubControlVer.java

--- src/main/java/com/mycompany/githubcontrolver/GithubControlVer.java
+++ src/main/java/com/mycompany/githubcontrolver/GithubControlVer.java
@@ -3,5 +3,6 @@ package com.mycompany.githubcontrolver;
 public class GithubControlVer {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Whats up!");
     }
 }
